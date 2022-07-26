e152b7b1fc0c5f6f77cac23457e96a11ec55ef6b
Noah Mayr <n.mayr@techdivision.com>
2022-07-26 16:43:42 +0200

    Add old buildpack as reference

---
+++ packages/pwa-buildpack-old/.eslintrc.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line node/no-unpublished-require
+const rootPkg = require('../../package.json');
+const rootModules = Object.keys(rootPkg.devDependencies).concat(
+    rootPkg.dependencies ? Object.keys(rootPkg.dependencies) : []
+);
+const uniqueRootModules = [...new Set(rootModules)];
+
+const config = {
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'script'
+    },
+    extends: ['@magento', 'plugin:node/recommended'],
+    plugins: ['babel', 'node'],
+    settings: {
+        node: {
+            allowModules: uniqueRootModules
+        }
+    },
+    rules: {
+        'no-prototype-builtins': 'off',
+        'no-undef': 'off',
+        'no-useless-escape': 'off',
+        "react/jsx-filename-extension": [1, { "allow": "as-needed" }]
+    }
+};
+
+module.exports = config;
