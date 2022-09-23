2795efb9429ae4b0ce999935f2c550607995b12c
Noah Mayr <n.mayr@techdivision.com>
2022-09-23 20:04:09 +0200

    Update dockerfile

--- .space.kts
+++ .space.kts
@@ -1,6 +1,6 @@
 // this job will run in a container based on the image
 // specified in the devfile.yaml (if specified)
-job("Build Idea indexes") {
+job("Build PhpStorm indexes") {
     warmup(ide = Ide.PhpStorm) {
         devfile = ".space/devfile.yaml"
     }
